875e0ef46469964dc0a5192a53d5873bdbffe1a4
Geovanny <agao6105@gmail.com>
2026-01-17 20:32:03 -0600

    feat: Implement initial mobile UI including layout, home page, search input, plant cards, and header components.

--- src/app/presentation/mobile/components/header/header.component.ts
+++ src/app/presentation/mobile/components/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton, IonMenuButton } from '@ionic/angular/standalone';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
     standalone: true,
-    imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton]
+    imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton, IonMenuButton]
 })
 export class HeaderComponent implements OnInit {
     @Input() title: string = '';
