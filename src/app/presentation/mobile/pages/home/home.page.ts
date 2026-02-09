e7c1fe485e14bdb22a8212960d118e768ddbfbea
Geovanny <agao6105@gmail.com>
2026-02-08 23:07:26 -0600

    feat: Add 'Most Searched Plants' page and enhance home page with plant of the day, most searched, and category sections.

--- src/app/presentation/mobile/pages/home/home.page.ts
+++ src/app/presentation/mobile/pages/home/home.page.ts
@@ -1,28 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonText, IonButton, IonSpinner, ModalController } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 import { CameraService } from '../../../../infrastructure/services/camera.service';
 import { IdentifyPlantUseCase } from '../../../../application/use-cases/identify-plant.use-case';
 import { Router } from '@angular/router';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { PlantCardComponent } from '../../components/plant-card/plant-card.component';
 
+import { Plant } from '../../../../domain/models/plant.entity';
+import { PlantRepository } from '../../../../domain/repositories/plant.repository';
+
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
     styleUrls: ['home.page.scss'],
     standalone: true,
-    imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonText, IonButton, IonSpinner, CommonModule, HeaderComponent, SearchInputComponent, PlantCardComponent],
+    imports: [IonContent, CommonModule, HeaderComponent, SearchInputComponent, PlantCardComponent],
 })
 export class HomePage {
     isScanning = false;
+    plants: Plant[] = [];
 
     constructor(
         private cameraService: CameraService,
         private identifyPlantUseCase: IdentifyPlantUseCase,
-        private router: Router
+        private router: Router,
+        private plantRepository: PlantRepository
     ) { }
+
+    ngOnInit() {
+        this.plantRepository.getPlants().subscribe(plants => {
+            this.plants = plants;
+        });
+    }
 
     onSearch(term: string) {
         console.log('Searching for:', term);
@@ -34,12 +45,12 @@ export class HomePage {
         // Implement filtering logic here
     }
 
-    viewDetail() {
-        this.router.navigate(['/detail-plant']);
+    viewDetail(id: string = '1') {
+        this.router.navigate(['/detail-plant', id]);
     }
 
     viewEnergizers() {
-        this.router.navigate(['/energizers']);
+        this.router.navigate(['/category-detail', 'Energizante']);
     }
 
     viewCategories() {
