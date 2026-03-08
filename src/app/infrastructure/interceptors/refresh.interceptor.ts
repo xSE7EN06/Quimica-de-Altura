b1085bba6bd74a94b7d5f6ca8eab4fcf8cc3a7be
Geovanny <agao6105@gmail.com>
2026-03-08 14:17:48 -0600

    feat: Add an HTTP interceptor for automatic token refreshing on 401 errors.

---
+++ src/app/infrastructure/interceptors/refresh.interceptor.ts
@@ -0,0 +1,60 @@
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
+import { catchError, filter, switchMap, take } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+import { TokenService } from '../services/token.service';
+
+let isRefreshing = false;
+let refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+export const refreshInterceptor: HttpInterceptorFn = (
+    req: HttpRequest<any>,
+    next: HttpHandlerFn
+): Observable<HttpEvent<any>> => {
+    const authService = inject(AuthService);
+    const tokenService = inject(TokenService);
+
+    return next(req).pipe(
+        catchError((error: HttpErrorResponse) => {
+            if (error instanceof HttpErrorResponse && error.status === 401) {
+                if (!isRefreshing) {
+                    isRefreshing = true;
+                    refreshTokenSubject.next(null);
+
+                    return authService.refreshToken().pipe(
+                        switchMap((tokenResponse: any) => {
+                            isRefreshing = false;
+                            refreshTokenSubject.next(tokenResponse.access_token);
+                            const clonedRequest = req.clone({
+                                setHeaders: {
+                                    Authorization: `Bearer ${tokenResponse.access_token}`
+                                }
+                            });
+                            return next(clonedRequest);
+                        }),
+                        catchError((err) => {
+                            isRefreshing = false;
+                            authService.logout();
+                            return throwError(() => err);
+                        })
+                    );
+                } else {
+                    return refreshTokenSubject.pipe(
+                        filter(token => token !== null),
+                        take(1),
+                        switchMap(token => {
+                            const clonedRequest = req.clone({
+                                setHeaders: {
+                                    Authorization: `Bearer ${token}`
+                                }
+                            });
+                            return next(clonedRequest);
+                        })
+                    );
+                }
+            }
+            return throwError(() => error);
+        })
+    );
+};
