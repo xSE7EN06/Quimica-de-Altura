288d78f2358709e0302caaeb535505e4d6f37454
Geovanny <agao6105@gmail.com>
2026-03-10 01:36:21 -0600

    feat: Implement mobile login and registration, including Google and biometric authentication.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.quimica.altura',
   appName: 'Quimica Altura',
-  webDir: 'dist/plant-id-app/browser'
+  webDir: 'dist/plant-id-app/browser',
+  server: {
+    cleartext: true
+  },
+  plugins: {
+    SocialLogin: {
+      providers: {
+        google: true
+      }
+    }
+  },
 };
 
 export default config;
